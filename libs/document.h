10e36c5224cc4118e1185d6fc08a3abcaa7987da
HaleyyT <150264185+HaleyyT@users.noreply.github.com>
2025-05-16 13:44:52 +1000

    fix the struct in document.h

--- libs/document.h
+++ libs/document.h
@@ -1,6 +1,7 @@
 #ifndef DOCUMENT_H
-
 #define DOCUMENT_H
+#include <stdint.h>
+
 /**
  * This file is the header file for all the document functions. You will be tested on the functions inside markdown.h
  * You are allowed to and encouraged multiple helper functions and data structures, and make your code as modular as possible. 
