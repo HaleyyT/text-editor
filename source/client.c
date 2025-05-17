a86d696e9933ed5e70f30c7c34d28a25759afdc5
HaleyyT <150264185+HaleyyT@users.noreply.github.com>
2025-05-17 12:16:34 +1000

    put main() in client.c to test make client

--- source/client.c
+++ source/client.c
@@ -1,3 +1,9 @@
 //TODO: client code that can send instructions to server.
+#include <stdio.h>
 
-
+int main(int argc, char *argv[]) {
+    (void)argc;
+    (void)argv;
+    printf("%s","client start ");
+    return 0;
+}
